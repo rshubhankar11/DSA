d336c37e46da5bd94c4617c4106a2cff2f94733c
rabiroshan shubhankar <rshubhankar11@gmail.com>
2022-07-02 16:37:10 +0530

    added coment to my first program

--- Programs/hellow.java
+++ Programs/hellow.java
@@ -1,10 +1,13 @@
 /**
- * hellow
+ * This is my frist program of DSA learning
+ * date 02-07-2022
+ * Today I have setup VS for Java so that I can use it to solve problems
+ * In VS I have added Estension Pack of java
  */
 public class hellow {
 
     public static void main(String[] args) {
         System.out.println("Hellow world");
     }
-    
+
 }
